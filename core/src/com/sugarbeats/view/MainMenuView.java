3c608e2463a825b602b6a82a33eae60d348247b4
taphan <trucphan307@gmail.com>
2018-04-13 11:45:15 +0200

    Fixed previously described problem, menu view almost ready, need graphics

--- core/src/com/sugarbeats/view/MainMenuView.java
+++ core/src/com/sugarbeats/view/MainMenuView.java
@@ -30,36 +30,30 @@ public class MainMenuView extends BaseView{
         this.game = game;
         this.controller = controller;
 
-        //guiCam = new OrthographicCamera(320, 480);
-        //guiCam.position.set(320 / 2, 480 / 2, 0);
         playBtn = new Texture("button_play.png");
         settingBtn = new Texture("button_settings.png");
         helpBtn = new Texture("button_help.png");
-        playBounds = new Rectangle(SugarBeats.WIDTH / 2 - playBtn.getWidth() / 2, SugarBeats.HEIGHT / 2, playBtn.getWidth(), playBtn.getHeight());
-        settingBounds = new Rectangle(SugarBeats.WIDTH / 2 + settingBtn.getWidth() / 2, SugarBeats.HEIGHT / 4, settingBtn.getWidth(), settingBtn.getHeight());
-        helpBounds = new Rectangle(SugarBeats.WIDTH / 2 - helpBtn.getWidth() * 3 / 2, SugarBeats.HEIGHT / 4, helpBtn.getWidth(), helpBtn.getHeight());
-        System.out.println(helpBounds.x + ", " + helpBounds.y);
+        playBounds = new Rectangle(SugarBeats.WIDTH / 2 - playBtn.getWidth() / 2, SugarBeats.HEIGHT - playBtn.getHeight() - SugarBeats.HEIGHT / 2, playBtn.getWidth(), playBtn.getHeight());
+        settingBounds = new Rectangle(SugarBeats.WIDTH / 2 + settingBtn.getWidth() / 2, SugarBeats.HEIGHT - settingBtn.getHeight() - SugarBeats.HEIGHT / 4, settingBtn.getWidth(), settingBtn.getHeight());
+        helpBounds = new Rectangle(SugarBeats.WIDTH / 2 - helpBtn.getWidth() * 3 / 2, SugarBeats.HEIGHT - helpBtn.getHeight() - SugarBeats.HEIGHT / 4, helpBtn.getWidth(), helpBtn.getHeight());
         touchPoint = new Vector3();
     }
 
     @Override
     public void update (float delta) {
         if (Gdx.input.justTouched()) {
-            // The rectangles are inverted on the screen compared to the actual buttons...
+            // Set touch point to check for whether a menu button has been pressed
             touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            System.out.println(touchPoint.x + ", " + touchPoint.y);
+
             if (playBounds.contains(touchPoint.x, touchPoint.y)) {
-                System.out.println("playBounds touched");
                 controller.onPlay();
                 return;
             }
             if (settingBounds.contains(touchPoint.x, touchPoint.y)) {
-                System.out.println("settingBounds" + touchPoint.x + touchPoint.y);
                 controller.onSettings();
                 return;
             }
             if (helpBounds.contains(touchPoint.x, touchPoint.y)) {
-                System.out.println("helpBounds"+ helpBounds.x+ helpBounds.y);
                 controller.onHelp();
             }
 
@@ -73,13 +67,13 @@ public class MainMenuView extends BaseView{
 
     @Override
     public void draw() {
+        // Draw menu buttons
+        // TODO: Add title picture/text and background
+        // TODO: Discuss about whether its necessary to constantly call the draw() function from render, is it enough to call it once during creation of class?
         game.batch.begin();
         game.batch.draw(playBtn, SugarBeats.WIDTH / 2 - playBtn.getWidth() / 2, SugarBeats.HEIGHT / 2);
         game.batch.draw(settingBtn, SugarBeats.WIDTH / 2 + settingBtn.getWidth() / 2, SugarBeats.HEIGHT / 4);
         game.batch.draw(helpBtn, SugarBeats.WIDTH / 2 - helpBtn.getWidth() * 3 / 2, SugarBeats.HEIGHT / 4);
-        // There is problem with the y coordinate used by draw method
-        // This one has origo on the bottom left corner, while the other one used for Rectangle
-        // and the rest of touch points are on the upper left corner
         game.batch.end();
     }
 
