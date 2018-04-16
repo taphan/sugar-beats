db19dd26f3ced98852da916d519858339eaa4037
taphan <trucphan307@gmail.com>
2018-04-16 10:17:57 +0200

    Potentially fixed main menu button problems

--- core/src/com/sugarbeats/view/MainMenuView.java
+++ core/src/com/sugarbeats/view/MainMenuView.java
@@ -1,6 +1,7 @@
 package com.sugarbeats.view;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
@@ -23,9 +24,10 @@ public class MainMenuView extends BaseView{
     Texture helpBtn;
     Vector3 touchPoint;
     MainMenuPresenter.ViewController controller;
+    OrthographicCamera cam;
 
-    private static final int WIDTH = SugarBeats.VIRTUAL_WIDTH;
-    private static final int HEIGHT = SugarBeats.VIRTUAL_HEIGHT;
+    private static final int WIDTH = SugarBeats.WIDTH;
+    private static final int HEIGHT = SugarBeats.HEIGHT;
     Integer height;
 
     public MainMenuView(SugarBeats game, MainMenuPresenter.ViewController controller) {
@@ -33,14 +35,16 @@ public class MainMenuView extends BaseView{
         this.game = game;
         this.controller = controller;
 
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, WIDTH, HEIGHT);
+
         playBtn = new Texture("button_play.png");
         settingBtn = new Texture("button_settings.png");
         helpBtn = new Texture("button_help.png");
-        playBounds = new Rectangle(WIDTH / 2 - playBtn.getWidth() / 2, HEIGHT - playBtn.getHeight() - HEIGHT / 2, playBtn.getWidth(), playBtn.getHeight());
-        settingBounds = new Rectangle(WIDTH / 2 + settingBtn.getWidth() / 2, HEIGHT - settingBtn.getHeight() - HEIGHT / 4, settingBtn.getWidth(), settingBtn.getHeight());
-        helpBounds = new Rectangle(WIDTH / 2 - helpBtn.getWidth() * 3 / 2, HEIGHT - helpBtn.getHeight() - HEIGHT / 4, helpBtn.getWidth(), helpBtn.getHeight());
+        playBounds = new Rectangle(WIDTH / 2 - playBtn.getWidth()/3 / 2, HEIGHT / 2, playBtn.getWidth() / 3, playBtn.getHeight() / 3);
+        settingBounds = new Rectangle(WIDTH / 2 + settingBtn.getWidth()/3 / 2, HEIGHT / 4, settingBtn.getWidth() / 3, settingBtn.getHeight() / 3);
+        helpBounds = new Rectangle(WIDTH / 2 - helpBtn.getWidth()/3 * 3 / 2, HEIGHT / 4, helpBtn.getWidth() / 3, helpBtn.getHeight() / 3);
         touchPoint = new Vector3();
-        System.out.println(playBtn.getWidth() + "" + playBtn.getHeight());
     }
 
     @Override
@@ -48,8 +52,8 @@ public class MainMenuView extends BaseView{
         if (Gdx.input.justTouched()) {
 
             // Set touch point to check for whether a menu button has been pressed
-            touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-
+            cam.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
+            System.out.println(touchPoint + ", y: " + (HEIGHT-touchPoint.y));
             if (playBounds.contains(touchPoint.x, touchPoint.y)) {
                 controller.onPlay();
                 return;
@@ -75,10 +79,12 @@ public class MainMenuView extends BaseView{
         // Draw menu buttons
         // TODO: Add title picture/text and background
         // TODO: Discuss about whether its necessary to constantly call the draw() function from render, is it enough to call it once during creation of class?
+        cam.update();
+        game.batch.setProjectionMatrix(cam.combined);
         game.batch.begin();
-        game.batch.draw(playBtn, WIDTH / 2 - playBtn.getWidth() / 2, HEIGHT / 2);
-        game.batch.draw(settingBtn, WIDTH / 2 + settingBtn.getWidth() / 2, HEIGHT / 4);
-        game.batch.draw(helpBtn, WIDTH / 2 - helpBtn.getWidth() * 3 / 2, HEIGHT / 4);
+        game.batch.draw(playBtn, WIDTH / 2 - playBtn.getWidth()/3 / 2, HEIGHT / 2, playBtn.getWidth() / 3, playBtn.getHeight() / 3);
+        game.batch.draw(settingBtn, WIDTH / 2 + settingBtn.getWidth()/3 / 2, HEIGHT / 4, settingBtn.getWidth() / 3, settingBtn.getHeight() / 3);
+        game.batch.draw(helpBtn, WIDTH / 2 - helpBtn.getWidth()/3 * 3 / 2, HEIGHT / 4, helpBtn.getWidth() / 3, helpBtn.getHeight() / 3);
         game.batch.end();
     }
 }
