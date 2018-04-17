51da102209d5c372da74ae721df721ae0164afe6
Siren Johansen <sirenjoh@hotmail.com>
2018-04-17 11:45:33 +0200

    Added Animationcomponent and system

---
+++ core/src/com/sugarbeats/game/entity/component/AnimationComponent.java
@@ -0,0 +1,12 @@
+package com.sugarbeats.game.entity.component;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.utils.IntMap;
+/**
+ * Created by sirenfj on 12.04.2018.
+ */
+
+public class AnimationComponent implements Component {
+    public IntMap<Animation> animations = new IntMap<Animation>();
+}
